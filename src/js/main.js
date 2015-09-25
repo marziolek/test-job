5c00a3fbef59e419dbf4320b2020f62d0e41bea4
Marcin Ziolek <mar.ziolek@gmail.com>
2015-09-25 15:57:45 +0200

    init done test

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+/* ==========================================================================
+
+    Project: job-test
+    Author: XHTMLized
+    Last updated: @@timestamp
+
+   ========================================================================== */
+
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+      App.galleryColorbox();
+      // App.feature2();
+    },
+
+    /**
+     * Custom feature 1
+     */
+    galleryColorbox: function() {
+      $(document).ready( function() {
+
+        $('.img-wrapper img').colorbox({
+          rel: 'gal',
+          href: function() {
+            return $(this).attr('src');
+          },
+          open: true
+        });
+
+        var slideshowCounter = 1;
+        $(document).bind('cbox_complete', function(){
+
+          if (slideshowCounter != 4) {
+            ++slideshowCounter;
+            setTimeout($.colorbox.next, 2000);
+          } else {
+            setTimeout($.colorbox.close, 2000);
+          }
+
+        });
+      });
+    },
+
+    /**
+     * Custom feature 2
+     */
+    feature2: function() {
+
+    }
+
+  };
+
+  $(function() {
+    App.init();
+  });
+
+})(jQuery);
